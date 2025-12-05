78ef681345b52cdde77ad57e3ba19d2c97fa1346
BeccaBorgmeier <rdb93159@creighton.edu>
2025-12-05 16:27:11 -0600

    Add files via upload

---
+++ 02-dev-tools-demo 2/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose a new endpoint for "name, favorite movie, music"
+
+    @GetMapping("/name")
+    public String getName() {
+        return "Name: Becca, Movie: Set it Up, Music: Pop";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+}
+
+
+
+
+
+
